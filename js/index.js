b26b6e8e86ac1188f294508d65f0b9830876916f
igu1 <eesaard@gmail.com>
2023-01-23 06:58:54 +0530

    Fun Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-AOS.init();
+
+async function getFun() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '66c21697f6mshb8c624d983eb822p10d6e7jsnd72ba434fb4c',
+            'X-RapidAPI-Host': 'jokeapi-v2.p.rapidapi.com'
+        }
+    };
+
+    var x = await fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json', options)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+
+    document.getElementById('fun-q').innerText = x['setup']
+    document.getElementById('fun-a').innerText = x['delivery']
+}
+
+getFun();
